fa480a1946051c4dcc901f07f48a2dffc95da709
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-07-26 19:07:25 -0700

    Needs nav work

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Theme Name: Han Wang's itc210 Demo
+Author: Han Wang of itc210 Class
+Author URI: http://han0919.com/
+Description: This is my demo theme for the itc210 WordPress class.
+Version: 1.0
+*/
+
+//Register Nav Menus
+register_nav_menus(array('main-menu'=>__('Main Menu')));
+
+register_nav_menus(array('maintwo-menu'=>__('Maintwo Menu')));
