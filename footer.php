6d3a2253a35d2417b190f8b6a5d26b04fe154758
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-07-31 17:49:53 -0700

    front-page work

--- footer.php
+++ footer.php
@@ -5,15 +5,18 @@
 			<img src="<?php bloginfo('template_directory'); ?>/emailsignup-fpo.png">
 		</div>
 		<div id="right">
-		<p>Get in touch: 206-777-7777</p>
-		<p>desideratarescue@gmail.com</p>
-		<img src="<?php bloginfo('template_directory'); ?>/icon-fpo.png">
-		<img src="<?php bloginfo('template_directory'); ?>/icon-fpo.png">
+		<p>From unwanted to love?</p>
+		
+		<img class="icon" src="<?php bloginfo('template_directory'); ?>/images/dog.png">
+		<img class="icon" src="<?php bloginfo('template_directory'); ?>/images/instagram.png">
+        <img class="icon" src="<?php bloginfo('template_directory'); ?>/images/facebook.png">
+            <p>Phone:206.777.7777</p>
+            <p>desideratarescue@gmail.com</p>
 	</div>
 		
-		<img class="square" src="<?php bloginfo('template_directory'); ?>/sponser-fpo.png">
-		<img class="square" src="<?php bloginfo('template_directory'); ?>/sponser-fpo.png">
-<img class="square" id="last" src="<?php bloginfo('template_directory'); ?>/sponser-fpo.png">
+		<img class="square" src="<?php bloginfo('template_directory'); ?>/images/rover.png">
+		<img class="square" src="<?php bloginfo('template_directory'); ?>/images/fred_meyer.png">
+        <img class="square" id="last" src="<?php bloginfo('template_directory'); ?>/images/badge.png">
 		<p>&copy All Rights Reserved to Desiderata Rescue</p>
         <small>front-page.php</small>
 	</div>
