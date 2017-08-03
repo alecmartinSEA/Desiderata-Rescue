4f0f12b41d389e9cfc7c26ab1aca5dca35adc9bb
ka0191 <sunga0191@gmail.com>
2017-08-02 18:12:40 -0700

    Nav fix lasdfj

--- header.php
+++ header.php
@@ -2,13 +2,14 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
 </head>
 <body>
     <header>
     <!--Test-->
-        
-        
-    
+
+
+
         <!--
         <ul id="nav-left">
             <li><a href="#">About</a></li>
@@ -16,20 +17,20 @@
                 <!--<ul class="sub-navigation">
             <li><a href="#">Dogs</a></li>
             <li><a href="#">Cats</a></li>
-        </ul> 
+        </ul>
     </li>
-         
+
     <li><a href="#">Foster</a></li>
 </ul>-->
 <img id="logo" src="<?php bloginfo('template_directory'); ?>/images/desiderata_logo.png">
 
-<?php wp_nav_menu(array('theme_location'=>'Main1', 'container' => false,  'items_wrap' => '<ul id="nav-left" 
+<?php wp_nav_menu(array('theme_location'=>'Main1', 'container' => false,  'items_wrap' => '<ul id="nav-left"
 class="%2$s">%3$s</ul>', ) ); ?>
-<img id="search-button" src="<?php bloginfo('template_directory'); ?>/images/search.png"> 
-   
+<img id="search-button" src="<?php bloginfo('template_directory'); ?>/images/search.png">
 
 
-<!--        
+
+<!--
 <ul id="nav-right">
     <li><a href="#">Volunteer</a></li>
     <li><a href="#">News</a></li>
@@ -39,4 +40,3 @@ class="%2$s">%3$s</ul>', ) ); ?>
 -->
 
 </header>
-	
