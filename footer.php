e548887725ae3d774bb370bebf5dfa50e195929e
nataliewang919 <hwang031@seattlecentral.edu>
2017-08-09 16:58:39 -0700

    add the footer plugin and style

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-
 	</div>
 	<div id="footer">
         <div class="footertop">
@@ -7,12 +6,80 @@
 		<img class="partners" src="<?php bloginfo('template_directory'); ?>/images/fred_meyer.png">
         <img class="partners" id="last" src="<?php bloginfo('template_directory'); ?>/images/badge.png">
         </div>
-        <div class="footerdown">
-		<div id="left">
-            <h3>Desiderate Rescue</h3>
-			<p>Sign up for news from Desiderata Rescue</p>
-			<img src="<?php bloginfo('template_directory'); ?>/emailsignup-fpo.png">
-		</div>
+
+<div class="footerdown">   
+<!-- style the form-->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+            
+<style type="text/css">
+    
+#mc_embed_signup{background:#9B7185; color:#FFFFFF;clear:left; font:14px Helvetica,Arial,sans-serif; width:50%; padding-left: 50px;}
+
+#mc-embedded-subscribe-form{ float:left;padding-left: 50px;}
+ 
+#mc_embed_signup form { text-align: left; padding:0px;}
+    
+#mc_embed_signup input.email {
+    font-family: "Open Sans","Helvetica Neue",Arial,Helvetica,Verdana,sans-serif;
+    font-size: 15px;
+    border: 1px solid #ABB0B2;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+    color: #343434;
+    background-color: #fff;
+    box-sizing: border-box;
+    height: 32px;
+    padding: 0px 0.4em;
+    display: inline-block;
+    margin: 0;
+    width: 250px;
+    vertical-align: top;
+}
+    
+#mc_embed_signup label {
+    display: block;
+    font-size: 14px;
+    padding-top: 10px;
+    padding-bottom: 20px;
+}
+
+#mc_embed_signup .button {
+    font-size: 13px;
+    border: none;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+    letter-spacing: .03em;
+    color: #636363;
+    background-color: #E0E0E0;
+    box-sizing: border-box;
+    height: 32px;
+    line-height: 32px;
+    padding: 0 5px;
+    display: inline-block;
+    margin: 0;
+    transition: all 0.23s ease-in-out 0s;
+}
+</style>
+<!--end style form-->
+    
+    <!--begin email signup form-->
+<div id="mc_embed_signup">
+<form action="//facebook.us16.list-manage.com/subscribe/post?u=a7163591a422f2e54822a61a4&amp;id=19edc6d318" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+        
+          <p class="singupheader">Desiderate Rescue</p>
+       
+	<label for="mce-EMAIL">Sign up for newsletter</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter your email" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_a7163591a422f2e54822a61a4_19edc6d318" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Sign up" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
 		<div id="right">
 		<p>From unwanted to love?</p>	
 		<a href="#"><img class="icon" src="<?php bloginfo('template_directory'); ?>/images/dog.png"></a>
