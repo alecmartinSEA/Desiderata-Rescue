99d503f7c035418ed24de1d13be5a5cf92f89469
Alec Martin <alecmartin28@gmail.com>
2017-08-11 15:45:08 -0700

    Started slide show

--- footer.php
+++ footer.php
@@ -94,6 +94,30 @@
         <small>front-page.php</small>
         </div>
     </div>
+    <script>
+    var slideIndex = 1;
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+    function timeSlides() {
+        window.setInterval("plusSlides(1)", 5000);
+    }
+    function showSlides(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        if (n > x.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = x.length}
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex-1].style.display = "block";
+    }
+    showSlides(slideIndex);
+    timeSlides();
+</script>
 	
 
 </body>
--- front-page.php
+++ front-page.php
@@ -1,22 +1,54 @@
 <?php get_header(); ?>
-	
+	<div class="slideImage">
+
+<div class="mySlides">
+  <img src="<?php bloginfo('template_directory'); ?>/images/slider1.png">
+</div>
+
+<div class="mySlides">
+  <img src="<?php bloginfo('template_directory'); ?>/images/slider2.png">
+</div>
+      
+<div class="mySlides">
+  <img src="<?php bloginfo('template_directory'); ?>/images/slider3.png">
+</div>  
+             
+<br>
+
+  <span  onclick="currentSlide(1)"></span> 
+  <span  onclick="currentSlide(2)"></span> 
+  <span onclick="currentSlide(3)"></span> 
+
+     
+        <div class="inner_box">
+         <h1>Desiderata Rescue</h1>
+         <p>From wanted to love</p><br>
+        
+        
+            <div class="button"> <a href="donate.html">DONATE</a></div>
+            </div><!--end inner_box-->
+            
+       </div> <!--end slideImages-->
    
 		<div id="content">
 			
-			<img class="slider" src="<?php bloginfo('template_directory'); ?>/images/slider1.png">
+
+
+
 			<div class="square">
                 <img src="<?php bloginfo('template_directory'); ?>/images/foster_icon.png">
-                <p>Lor em ipsum dolor sit amet, </br>eam  intellegam ne.</p>
+                <p>Want to adopt but can't commit? </br>Here's a soultion</p>
                 <button>foster</button>
             </div>
             <div class="square">
                 <img src="<?php bloginfo('template_directory'); ?>/images/adoption_icon.png">
-                <p>Lor em ipsum dolor sit amet, </br>eam  intellegam ne.</p>
+                <p>Browse our cats and dogs </br> avaliable for adoption today</p>
                 <button>adopt</button>
             </div>
             <div class="square" id="last">
                 <img src="<?php bloginfo('template_directory'); ?>/images/donation_icon.png">
-                <p>Lor em ipsum dolor sit amet, </br>eam  intellegam ne.</p>
+                <p>Anything helps</br>  &#x2764;</p>
+                
                 <button>donate</button>
             </div>
         </div>
