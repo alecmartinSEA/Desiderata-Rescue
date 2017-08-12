8e8b770ac72b723bc0f5b6cf4309a0ed0338106f
ka0191 <sunga0191@gmail.com>
2017-08-11 17:19:17 -0700

    Add files via upload
    adopt.php!!

--- adopt.php
+++ adopt.php
@@ -1,40 +1,41 @@
 <?php /* Template Name: adopt */ ?>
 <?php get_header(); ?>
+
+
 <div class="mainadopt">
 <div class="adopttop">
-<h3>Desiderata Adoption</h3>
-<img class="adopttoppic" src="<?php bloginfo('template_directory'); ?>/images/adop_1.png">
-<p>Your pet brings you so much happiness and joy. </p>
-<p>Your gift will help us complete the shelter and make pet adoption possible for families just like yours.</p>
-    <div class="button1">
-<button class="buttonclick1" type="button" onclick="location.href='http://google.com';">Dowload Adoption Form</button>  
-        </div>
+    <h3>Desiderata Adoption</h3>
+    <img class="adopttoppic" src="images/adop_1.png">
+    <p class="txt_top">Your pet brings you so much happiness and joy. Your gift will help us complete the shelter and make pet adoption possible for families just like yours.</p>
+
+    <button class="buttonclick1" type="button" onclick="location.href='http://google.com';">Dowload Adoption Form</button>
+
 </div>
-    
+
 <div class="adoptmiddle">
 
-<div class="dogsection">
-<div class="adoptpic">
-<img class="adoptdog" src="<?php bloginfo('template_directory'); ?>/images/adop_dog.png">  
-</div>
-<div class="adoptword">
-<h3>Dogs</h3> 
-<p>Lorem ipsum dolor sit amet, eam intellegam ne. Lorem ipsum dolor sit amet, eam intellegam ne.</p>   
- <button class="buttonclick2" type="button" onclick="location.href='http://google.com';">Woof Woof</button>      
-</div>  
-</div>
+    <div class="sec">
+        <div class="adoptpic">
+            <img class="adoptdog" src="images/adopt_dog.png">
+        </div>
+        <div class="adoptword">
+            <h3>Dogs</h3>
+            <p>Lorem ipsum dolor sit amet, eam intellegam ne. Lorem ipsum dolor sit amet, eam intellegam ne.</p>
+             <button class="buttonclick2" type="button" onclick="location.href='http://google.com';">Woof Woof</button>
+        </div>
+    </div>
 
-<div class="catsection">
-<div class="adoptcatword">
-<h3>Cats</h3> 
-<p>Lorem ipsum dolor sit amet, eam intellegam ne. Lorem ipsum dolor sit amet, eam intellegam ne.</p>
-<button class="buttonclick2" type="button" onclick="location.href='http://google.com';">Meow Meow</button>
-</div>
-    <div class="adoptcatpic">
-<img  class="adoptdog" src="<?php bloginfo('template_directory'); ?>/images/adop_cat.png">  
-</div>
-</div>
-    
+    <div class="sec cat_responsive">
+        <div class="adoptcatword">
+        <h3>Cats</h3>
+        <p>Lorem ipsum dolor sit amet, eam intellegam ne. Lorem ipsum dolor sit amet, eam intellegam ne.</p>
+        <button class="buttonclick2" type="button" onclick="location.href='http://google.com';">Meow Meow</button>
+        </div>
+            <div class="adoptcatpic">
+        <img  class="adoptdog" src="images/adopt_cat.png">
+        </div>
+    </div>
+
 </div>
 
 
