1b9d27de9d2b3e9e68a2f60f7efb5e7e97f1295a
Alec Martin <alecmartin28@gmail.com>
2017-08-14 17:01:55 -0700

    styled dog/cat page

--- cats.php
+++ cats.php
@@ -1,6 +1,8 @@
 <?php /* Template Name: cats */ ?>
 <?php get_header(); ?>
-<div id="content">
+<div class="mainadopt" style="text-align: center;">
+	<h3>Adoptable Cats</h3>
+<div id="content">	
 <script language="JavaScript">
 var AAPPetScrollerSettings = {
    'searchtools_box_width': '450',
@@ -13,7 +15,8 @@ var AAPPetScrollerSettings = {
    'sort_by': 'pet_name'
 };</script>
 <script language="JavaScript" src="https://dq25e8j0im0tm.cloudfront.net/js/st-portable-pet-list.js"></script>
-<div style="background-color:#fff; width:450px; text-align:center; height:53px;"><p style="font-family:Helvetica, Arial, Geneva, sans-serif; font-size:12px; color: #444444
+<div style="background-color:#fff; width:450px; text-align:center; height:53px; margin: 0 auto;"><p style="font-family: 'Nunito Sans', sans-serif; font-size:12px; color: #444444
 ;">Pet adoption and rescue powered by <a href="http://www.adoptapet.com/" title="Pet adoption and rescue powered by Adopt-a-Pet.com"><img src="https://dq25e8j0im0tm.cloudfront.net/images/st-logo-lg.gif" alt="Adopt-a-Pet.com" width="150" height="37" border="0" style="vertical-align:middle; margin-left:15px;" /></a></p></div>
+</div>
 </div>
 <?php get_footer(); ?>
--- dog.php
+++ dog.php
@@ -1,5 +1,7 @@
 <?php /* Template Name: dogs */ ?>
 <?php get_header(); ?>
+<div class="mainadopt" style="text-align: center;">
+	<h3>Adoptable Dogs</h3>
 <div id="content">
 <script language="JavaScript">
 var AAPPetScrollerSettings = {
@@ -13,7 +15,7 @@ var AAPPetScrollerSettings = {
    'sort_by': 'pet_name'
 };</script>
 <script language="JavaScript" src="https://dq25e8j0im0tm.cloudfront.net/js/st-portable-pet-list.js"></script>
-<div style="background-color:#fff; width:450px; text-align:center; height:53px;"><p style="font-family:Helvetica, Arial, Geneva, sans-serif; font-size:12px; color: #444444
+<div style="background-color:#fff; width:450px; text-align:center; height:53px; margin: 0 auto;"><p style="font-family:Helvetica, Arial, Geneva, sans-serif; font-size:12px; color: #444444
 ;">Pet adoption and rescue powered by <a href="http://www.adoptapet.com/" title="Pet adoption and rescue powered by Adopt-a-Pet.com"><img src="https://dq25e8j0im0tm.cloudfront.net/images/st-logo-lg.gif" alt="Adopt-a-Pet.com" width="150" height="37" border="0" style="vertical-align:middle; margin-left:15px;" /></a></p></div>
 </script>
 </div>
