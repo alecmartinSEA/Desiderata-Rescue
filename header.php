d9bc64a62ad112f5079d5de49e129af69c8065f9
Alec Martin <alecmartin28@gmail.com>
2017-08-18 17:02:02 -0700

    No search

--- header.php
+++ header.php
@@ -6,7 +6,34 @@
    <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0">
    <!-- Begin Toggle Menu -->
+<script type="text/javascript" charset="utf-8">
+  $(window).load(function() { // enable function upon window load
+$("#toggle").click(function() {
+$("#mobile-nav").slideToggle(); // toggle main-menu
+return false; // disable anchor tag target
+});
 
+var loadWidth = window.innerWidth; // save window load width as a variable
+
+$(window).resize(function() { // when the window is resized...
+
+if ( loadWidth !== window.innerWidth ) { // trigger for width only...
+
+if (window.innerWidth < 801) { // if width is less than 801px...
+
+$("#mobile-nav").hide(); // hide main navigation items
+
+} else { // else ...
+
+$("#mobile-nav").show(); // show main navigation items
+}
+
+} // end trigger for width only
+
+}); // end window resize
+
+}); // end window load
+</script>
 
 
 
@@ -39,9 +66,7 @@
 <div id="toggle">
 <a href="#"><span class="glyph">&#8801;</span>&nbsp;</a>
 </div>
-<img id="search-button" src="<?php bloginfo('template_directory'); ?>/images/search.png">
-<?php wp_nav_menu(array('theme_location'=>'Main2', 'container' => false,  'items_wrap' => '<ul id="mobile-nav"
-class="hide">%3$s</ul>', ) ); ?>
+
 
 <?php wp_nav_menu(array('theme_location'=>'Main1', 'container' => false,  'items_wrap' => '<ul id="nav-left"
 class="hide">%3$s</ul>', ) ); ?>
@@ -59,13 +84,7 @@ class="hide">%3$s</ul>', ) ); ?>
 
 
 
-<?php
-if ( is_active_sidebar( 'custom-header-widget' ) ) : ?>
-    <div id="header-widget-area" class="chw-widget-area widget-area show-search" role="complementary">
-    <?php dynamic_sidebar( 'custom-header-widget' ); ?>
-    </div>
-    
-<?php endif; ?>
+
 
 
 
